88de8a2ad077642973573a07317212ec2920f3ad
euanthieu <euanthieu@gmail.com>
2024-06-20 23:38:35 +0800

    Update: Added Routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AboutMeController;
+use App\Http\Controllers\SkillsController;
+use App\Http\Controllers\HobbiesController;
 
+//Home Page
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+// About Me Route
+Route::get('/about_me', [AboutMeController::class, 'show']);
+
+// Skills Route
+Route::get('/skills', [SkillsController::class, 'show']);
+
+// Hobbies Route
+Route::get('/hobbies', [HobbiesController::class, 'show']);
