27c9ccda22c54b3046cb496fa4ccd71649f5acff
euanthieu <euanthieu@gmail.com>
2024-06-20 23:29:17 +0800

    Update: Added Views

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Annyeong</title>
+</head>
+<body>
+    <h1>Annyeong Saranghae!</h1>
+    <p>Discover who this is.</p>
+    <nav>
+        <a href="/about_me"><button>About Me</button></a>
+        <a href="/skills"><button>Skills</button></a>
+        <a href="/hobbies"><button>Hobbies</button></a>
+    </nav>
+</body>
+</html>
